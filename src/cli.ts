3c3b110591428d7571ed8161b022832608db47c5
Dan Cox <dan.cox@videlais.com>
2025-11-27 17:42:52 -0600

    Updating CLI and package

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name('cm-packer')
   .description('CLI tool for packing and unpacking IMSCC files (Common Cartridge 1.4)')
-  .version('1.0.0');
+  .version('1.0.3');
 
 program
   .command('unpack')
